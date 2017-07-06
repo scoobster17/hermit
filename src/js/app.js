b131a78aab240fea797c3608b297aaea4cff91ce
Phil Gibbins <scoobster17@hotmail.co.uk>
2017-07-06 12:00:49 +0100

    Refactoring of fetching preconfigured sites, adjusted button click event to utilise event bubbling, added generated files to git ignore

--- src/js/app.js
+++ src/js/app.js
@@ -22,8 +22,11 @@ import { serializeForm } from './utils/formHandling';
  * BINDING ELEMENTS
  */
 
-// list of configurations on teh hermit tab
-const preconfiguredTabs = document.getElementById('pre-configured-tabs');
+// section containing list of pre-configured sites
+const preconfiguredTabsContainer = document.getElementById('pre-configured-tabs');
+
+// list of configurations on the hermit tab
+const preconfiguredTabs = document.getElementById('pre-configured-tabs-list');
 
 // tabs
 const tabList = document.getElementById('hermit-nav');
@@ -37,52 +40,78 @@ const dynamicTabsContentContainer = document.getElementById('dynamic-tabs-conten
 /******************************************************************************/
 
 /**
- * BINDING ELEMENTS
+ * MAIN FUNCTIONALITY
  */
 
+/**
+ * Get all configurations for supported sites from the local plugins folder
+ */
 const getPreconfiguredTabs = () => {
     fetch('/pre-configured-tabs')
     .then(res => {
-        if (res.ok) return res;
-        throw new Error('There was an error');
+        if (res.ok) return res.json();
+        throw new Error('There was an error fetching pre-configured tabs');
     })
-    .then(res => res.json())
-    .then(res => {
-        let html = '';
-        res.forEach((siteConfig) => {
-            html += [
-                '<li>',
-                    '<button ',
-                        `data-name="${ siteConfig.name }" `,
-                        `data-site="${ siteConfig.site }" `,
-                        `data-url="${ siteConfig.url }" `,
-                    '>',
-                        siteConfig.name,
-                        `<img src="img/service/${ siteConfig.name.toLowerCase() }/${ siteConfig.logo }" alt="" />`,
-                    '</button>',
-                '</li>'
-            ].join('');
-        });
-        preconfiguredTabs.innerHTML = html;
-        var addNewSiteSection = document.getElementById('add-new-site');
-        var buttons = preconfiguredTabs.getElementsByTagName('button');
-        for (let button of buttons) {
-            button.addEventListener('click', function(event) {
-                for (let attr in event.target.dataset) {
-                    const formInput = document.getElementById(attr);
-                    if (formInput) {
-                        formInput.value = event.target.dataset[attr];
-                    }
-                }
-                // attempt at UX for now (TODO smooth scroll)
-                addNewSiteSection.scrollIntoView();
-            });
-            const buttonImg = button.querySelector('img');
-            if (buttonImg) button.addEventListener('click', (event) => {
-                event.target.parentElement.click();
-            });
-        }
+    .then(addPreconfiguredSiteButtons)
+    .catch(err => {
+        console.error(err);
+
+        // if there is an error, simply hide the section providing this
+        // functionality, as it is not available
+        preconfiguredTabsContainer.setAttribute('aria-hidden', 'true');
     });
+};
+
+/**
+ * Add buttons to the DOM for site configurations found in the local file system
+ * @param  {Array} siteConfigs  An array of site configuration objects found in
+ *                              the local file system, which can pre-populate
+ *                              the form to add a new tab
+ */
+const addPreconfiguredSiteButtons = (siteConfigs) => {
+
+    let html = '';
+    const addNewSiteSection = document.getElementById('add-new-site');
+
+    // loop through the list of configurations, appending the button template
+    // with dynamic data to the html variable
+    siteConfigs.forEach((siteConfig) => {
+        html += [
+            '<li>',
+                '<button ',
+                    `data-name="${ siteConfig.name }" `,
+                    `data-site="${ siteConfig.site }" `,
+                    `data-url="${ siteConfig.url }" `,
+                '>',
+                    siteConfig.name,
+                    `<img src="img/service/${ siteConfig.name.toLowerCase() }/${ siteConfig.logo }" alt="" />`,
+                '</button>',
+            '</li>'
+        ].join('');
+    });
+
+    // append this html to the DOM
+    preconfiguredTabs.innerHTML = html;
+
+    // once the HTML has been appended to the DOM, find the button elements
+    const buttons = preconfiguredTabs.getElementsByTagName('button');
+
+    // loop through the appended buttons and add a click event to populate form
+    for (let button of buttons) {
+        button.addEventListener('click', function(event) {
+
+            // loop through the data- attributes and fill the matching field
+            for (let attr in button.dataset) {
+                const formInput = document.getElementById(attr);
+                if (formInput) {
+                    formInput.value = button.dataset[attr];
+                }
+            }
+
+            // attempt at UX for now (TODO smooth scroll)
+            addNewSiteSection.scrollIntoView();
+        });
+    }
 };
 
 const bindTabTriggers = () => {
